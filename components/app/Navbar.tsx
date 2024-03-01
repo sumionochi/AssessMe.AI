f520b9b1e5c88ca9d7ba92f851c2cb3468ad0ae5
Samarth Saxena <samarthsaxena1672003@gmail.com>
2024-03-01 17:45:38 +0530

    slight navbar modification

--- components/app/Navbar.tsx
+++ components/app/Navbar.tsx
@@ -1,20 +1,20 @@
 import { lexend } from "@/app/fonts";
 import { dark } from "@clerk/themes";
-
 import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu";
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 import { UserButton } from "@clerk/nextjs";
 import { Button } from "../ui/button";
+import { MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes";
 
 export default function Navbar() {
+  const { setTheme } = useTheme();
+
   return (
     <div className="flex h-[4.5rem] w-full bg-[#28736d] items-center justify-between  fixed  top-0 px-3 py-2">
       <div className={"text-2xl self font-bold " + lexend.className}>
@@ -30,8 +30,32 @@ export default function Navbar() {
         <Button className="bg-[#256963] text-white hover:bg-[#194240]">
           Analytics
         </Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="outline"
+              className="bg-[#256963] hover:bg-[#1c4a47] p-0 mr-1 border-0"
+              size="icon"
+            >
+              <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
 
-        <div className="h-20 items-center flex">
+        <div className="h-20  items-center flex">
           <UserButton
             showName={false}
             appearance={{
