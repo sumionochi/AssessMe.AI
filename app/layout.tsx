6f551e89de8e52b1535253964e6dac9619c04856
sumionochi <aaditya.srivastava.connect@gmail.com>
2024-03-15 10:17:41 +0530

    removed unwanted files and changes the ui for home and dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,51 +1,39 @@
 // src/app/layout.tsx
-import type { Metadata } from "next";
-import { Lexend, Montserrat } from "next/font/google";
-import "./globals.css";
-import { cn } from "@/lib/utils";
-import { ThemeProvider } from "@/components/ui/ThemeProvider";
-import { ClerkProvider, auth } from "@clerk/nextjs";
-import { dark, neobrutalism } from "@clerk/themes";
-import { Toaster } from "@/components/ui/sonner";
-import { inter } from "./fonts";
+import type { Metadata } from 'next'
+import { Lexend } from 'next/font/google'
+import './globals.css'
+import { cn } from '@/lib/utils'
+import { ThemeProvider } from '@/components/ui/ThemeProvider';
+import { ClerkProvider, auth } from "@clerk/nextjs"
+import { dark, neobrutalism } from '@clerk/themes';
+import { Toaster } from "@/components/ui/sonner"
+import { ConvexClientProvider } from '@/providers/convex-client-provider';
 import NavHeader from '@/components/NavHeader';
-import { ConvexClientProvider } from "@/providers/convex-client-provider";
 
 const lexend = Lexend({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'RecruitMe.AI',
+  title: 'AssessMe.AI',
   description: 'Enhance Interview Skills Using AI.',
 }
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
-  const { userId } = auth();
+  const {userId} = auth();
   return (
-<html lang="en" suppressHydrationWarning={true}>
-      <body
-        className={cn(
-          inter.className,
-          "antialiased min-h-screen border-none outline-none",
-          "scrollbar scrollbar-thumb scrollbar-thumb-white scrollbar-track-slate-700 bg-[#222629] "
-        )}
-        suppressHydrationWarning={true}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <ConvexClientProvider> {children} </ConvexClientProvider>
-          <Toaster />
-        </ThemeProvider>
-      </body>
-    </html>
+      <html lang="en" suppressHydrationWarning={true}>
+        <body className={cn(lexend.className, 'antialiased min-h-screen border-none outline-none', 'scrollbar scrollbar-thumb scrollbar-thumb-white scrollbar-track-slate-700 bg-gradient-to-tl from-violet-400 to-violet-300 dark:bg-gradient-to-br dark:from-gray-900 dark:via-purple-900 dark:to-violet-600')} suppressHydrationWarning={true}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            <ConvexClientProvider>
+              <NavHeader userId={userId}/>
+              {children}
+            </ConvexClientProvider>
+            <Toaster/>
+          </ThemeProvider>
+        </body>
+      </html>
   )
-    
-  
 }
