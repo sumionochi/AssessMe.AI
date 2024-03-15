bf7d8aa9c265e09367699523b8b5aa651eea0db0
sumionochi <aaditya.srivastava.connect@gmail.com>
2024-03-15 10:18:56 +0530

    created the dashboard with assessment card creation

---
+++ components/EveryAssessmentCard.tsx
@@ -0,0 +1,20 @@
+"use client"
+import { api } from '@/convex/_generated/api';
+import { useQuery } from 'convex/react';
+import React from 'react'
+import AssessmentDisplay from './AssessmentDisplay';
+
+type Props = {}
+
+const EveryAssessmentCard = (props: Props) => {
+    const EveryAssessment = useQuery(api.assess.get);
+  return (
+    <>
+    {EveryAssessment?.map((assessment) => (
+        <AssessmentDisplay assessment={assessment} key={assessment._id} />
+      ))}
+    </>
+  )
+}
+
+export default EveryAssessmentCard
